d5a1e94e8f07b19ee640da0ac88bf0fea83a988e
deema <dandreev@dataeast.com>
2015-02-14 18:15:48 +0600

    use constants

--- yokta.pl
+++ yokta.pl
@@ -1,19 +1,20 @@
 #!/usr/bin/perl -w
 
+use strict;
 use MIME::Base32 qw( RFC );
 use Digest::SHA qw( hmac_sha1 );
 
-my $okta_secret = "KFWXIQLGNJLTQMCX";
+use constant OKTA_SECRET => "KFWXIQLGNJLTQMCX";
 
 ##################################################
 
 sub yokta_code {
-    my $time_step = 30;
-    my $passcode_length = 6;
-    my $modulo = 10 ** $passcode_length;
+    use constant TIME_STEP    => 30;
+    use constant PASSCODE_LEN => 6;
+    use constant MODULO       => 10 ** PASSCODE_LEN;
 
     my $secret = $_[0];
-    my $interval = int(time() / $time_step);
+    my $interval = int(time() / TIME_STEP);
 
     my $hash = hmac_sha1(
         pack("Q>", $interval),
@@ -21,24 +22,16 @@ sub yokta_code {
 
     my @hash_bytes = unpack("(C)*", $hash);
 
-    #print "hash: ";
-    #printf("%#02x ", $_) for @hash_bytes;
-    #print "\n";
-
     my $offset = $hash_bytes[-1] & 0xf;
     my @int_data = splice(@hash_bytes, $offset, 4);
 
-    #print "int bytes: ";
-    #printf("%#02x ", $_) for @int_data;
-    #print "\n";
-
     my $int_code = unpack("L>", pack("C4", @int_data));
-    $int_code = (0x7fffffff & $int_code) % $modulo;
+    $int_code = (0x7fffffff & $int_code) % MODULO;
 
-    return sprintf(sprintf("%%0%dd", $passcode_length), $int_code);
+    return sprintf("%0".PASSCODE_LEN."d", $int_code);
 }
 
-my $code = yokta_code($okta_secret);
+my $code = yokta_code(OKTA_SECRET);
 
 print "$code\n";
 
